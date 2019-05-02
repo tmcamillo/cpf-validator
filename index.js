f334e4e6c1b24063aae5e0b2849471564cb2afa1
tmcamillo <imeio.tmc@gmail.com>
2019-05-02 15:54:05 -0300

    refatoração de nomes de variáveis e função de array, substituição de let por const, deletado comentários

--- index.js
+++ index.js
@@ -1,63 +1,44 @@
-// 1limpar mascara
-// 2validar tamanho
-// 3validar repeticao
-// !4validar se nao eh sequencia
-// 5procurar digitos
-// 6digito1
-// 7digito2
-// 8validar cpf
-
-
 const cleanCpf = (cpf) => {
-
   return cpf.toString().replace(/[^\d]+/g, '');
-
-};
-
-const isRepeatingChars = (cpf) => {
-
-  return cpf.split('').every((elem) => elem === cpf[ 0 ]);
-
 };
 
 const convertCpfToArray = (cpf) => {
-
   return Array.from(cpf);
-
 };
 
-const verifierDigit = (cpf, index) => {
-
-  let sum = cpf.slice(0, (index - 1));
-  let mult = sum.map(element => element * index--);
-  let total = mult.reduce((accumulator, currentValue) => accumulator + currentValue);
-  let modTotal = (total * 10) % 11;
-  return modTotal;
-
+const isRepeatingChars = (cpf) => {
+  return convertCpfToArray(cpf).every((elem) => elem === cpf[ 0 ]);
 };
 
-const testerDigit = (digit) => {
+const checkDigit = (cpf, initialNumber) => {
+  const sliced = cpf.slice(0, (initialNumber - 1));
+  const multiply = sliced.map(element => element * initialNumber--);
+  const total = multiply.reduce((accumulator, currentValue) => (accumulator + currentValue)) * 10 % 11;
+  return total;
+};
 
-  return (digit > 9) ? 0 : digit;
-
+const validateDigit = (digit) => {
+  return (digit === 10 || digit === 11) ? 0 : digit;
 };
 
 const cpfValidator = (cpf) => {
-
   const cleanedCpf = cleanCpf(cpf); 
   const arrayCpf = convertCpfToArray(cleanedCpf);
-  if (cleanedCpf.length !== 11 || isRepeatingChars(cleanedCpf)) return false;
+  if (cleanedCpf.length !== 11 || isRepeatingChars(cleanedCpf)) {
+    return false;
+  }
 	
-  let digit1 = verifierDigit(arrayCpf, 10);
-  digit1 = testerDigit(digit1);
+  let digit1 = checkDigit(arrayCpf, 10);
+  digit1 = validateDigit(digit1);
 	
-  let digit2 = verifierDigit(arrayCpf, 11);
-  digit2 = testerDigit(digit2);
+  let digit2 = checkDigit(arrayCpf, 11);
+  digit2 = validateDigit(digit2);
 	
-  if (digit1 !== parseInt(arrayCpf[ 9 ]) || digit2 !== parseInt(arrayCpf[ 10 ])) return false;
+  if (digit1 !== parseInt(arrayCpf[ 9 ]) || digit2 !== parseInt(arrayCpf[ 10 ])) {
+    return false;
+  }
 	
   return true;
-
 };
 
 module.exports = cpfValidator;
